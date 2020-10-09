5d32ff80b23df3d92e9f97ebecbcdcb5ca42c697
Ma Tinyau <0xmty.007@gmail.com>
2020-10-09 12:11:44 +0800

    using

--- using/main.cpp
+++ using/main.cpp
@@ -8,26 +8,28 @@ namespace ASpace
 	class B
 	{
 	public:
+		class BInternal;
+
+	public:
 		virtual void f(int) { std::cout << "B::f\n"; }
 		void g(char) { std::cout << "B::g\n"; }
 		void h(int) { std::cout << "B::h\n"; }
 
 	protected:
 		int m; // B::m is protected
-		typedef int value_type;
+		using value_type = int;
 	};
 }
 
 namespace BSpace
 {
-	using namespace ASpace;
-	using B_Alias = B;
+	using B_Alias = ASpace::B;
 
-	class D : B_Alias
+	class D : public B_Alias
 	{
 	public:
 		using B::m;										// D::m is public
-		using B::value_type;							// D::value_type is public
+		using B::value_type;
 
 		using B::f;
 		void f(int) { std::cout << "D::f\n"; }		// D::f(int) overrides B::f(int)
@@ -38,11 +40,34 @@ namespace BSpace
 		void h(int) { std::cout << "D::h\n"; }	// D::h(int) hides B::h(int)
 	};
 }
+//-----------------------------------------------------------------------------------
 
 //Type alias, alias template (since C++11)
 //https://en.cppreference.com/w/cpp/language/type_alias
 template<typename T>
 using memberf_pointer = void (T::*)(void);
+
+//模板套模板 (since C++14)
+template<typename T>
+using vec_member_pointer = std::vector<memberf_pointer<typename T::BInternal>>;
+
+//-----------------------------------------------------------------------------------
+
+//模板的 specializing 必须是有穷展开
+template<class T>
+struct A;
+template<class T>
+using B = typename A<T>::U; // type-id is A<T>::U
+
+template<class T>
+struct A 
+{
+	struct AInternal;
+	typedef B<T> U;
+};
+//B<int> b; //error
+
+//-----------------------------------------------------------------------------------
 
 int main()
 {
@@ -61,6 +86,8 @@ int main()
 	using BSpace::D;
 	D d;
 	d.m = 1;
+
+	vec_member_pointer<D> vec_d_pointer;
 
 	return 0;
 }
